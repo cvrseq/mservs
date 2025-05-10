3345d5f871eadf2ceb7dcf2da8757d3927e88962
anger issues <skuenemy@gmail.com>
2025-05-10 21:25:48 +0300

    update

--- microservices/api-service/cmd/main.go
+++ microservices/api-service/cmd/main.go
@@ -15,10 +15,10 @@ func main() {
 		"tag":  "<br>",
 	  }
   
-	  // will output : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+	  
 	  c.AsciiJSON(http.StatusOK, data)
 	})
   
-	// Listen and serve on 0.0.0.0:8080
-	r.Run(":8080")
+	
+	r.Run(":8000")
   }
